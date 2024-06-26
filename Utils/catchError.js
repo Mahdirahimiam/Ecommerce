c37c90cc2a0f2e42ea967f54a96f3dee4980c192
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-06-26 10:21:56 +0330

    Create catchError.js

---
+++ Utils/catchError.js
@@ -0,0 +1,12 @@
+const catchError = (err,req,res,next)=>{
+    console.log(err);
+    err.statusCode=err.statusCode || 500;
+    err.status=err.status|| 'error'
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    })
+
+}
+
+export default catchError
