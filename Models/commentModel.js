15cd1163c1e3c4b8d0dfe99cdcf3c3af40a71872
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-06 10:27:43 +0330

    Update commentModel.js

---
+++ Models/commentModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: [true, "نوشت پیام اجباری است"],
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  },
+  { timestamps: true }
+);
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
