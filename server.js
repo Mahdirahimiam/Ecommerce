50935424b7440308e0aa482d6fcc4d9711bfcefc
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-06 12:01:40 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import app from "./app.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
-mongoose.connect(process.env.DATA_BASE).then(() => {
-  console.log("DATABASE IS CONNECT");
-});
+mongoose
+  .connect(process.env.DATA_BASE)
+  .then(() => {
+    console.log("DATABASE IS CONNECT");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is run`);
