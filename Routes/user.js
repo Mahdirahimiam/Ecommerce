6c5d356761490a0ce6cfb2635ded498faba65283
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-07 16:48:05 +0330

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -1,11 +1,7 @@
 import express from "express";
+import { getAllUser, getOneUser } from "../Controllers/userCn.js";
 const userRouter = express.Router();
+userRouter.route("/").get(getAllUser);
+userRouter.route("/:id").get(getOneUser);
 
-
-
-
-
-
-
-
-export default userRouter
+export default userRouter;
