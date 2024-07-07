f830d9d5c710676420c3634f2a6347005438b045
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-07 13:46:57 +0330

    Update auth.js
    add route

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,6 +1,8 @@
 import express from "express";
+import {  checkOtp, otp } from "../Controllers/authCn.js";
 const authRouter = express.Router();
-
+authRouter.route('/').post(otp)
+authRouter.route('/otp').post(checkOtp)
 
 
 
