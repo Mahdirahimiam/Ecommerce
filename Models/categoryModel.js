946ccb6ccbd454b3979df677262552aaf74d9f1e
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-08 12:19:32 +0330

    Update categoryModel.js
    add href in category model

--- Models/categoryModel.js
+++ Models/categoryModel.js
@@ -3,6 +3,9 @@ const categorySchema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "نام دسته بندی الزامی است"] },
     image: { type: String, required: [true, "عکس برای دسته بندی الزامی است"] },
+    href:{
+      type:String,
+    },
     subCategory: [
       {
         type: mongoose.Schema.Types.ObjectId,
