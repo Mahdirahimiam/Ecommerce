a9d17e64d1eef4539a09f936202914a0e7fcd2ee
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-08 12:19:42 +0330

    Update category.js

--- Routes/category.js
+++ Routes/category.js
@@ -1,11 +1,21 @@
 import express from "express";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategory,
+  getOneCategory,
+  updateCategory,
+} from "../Controllers/categoryCn.js";
+import upload from "../Utils/UploadFile.js";
 const categoryRouter = express.Router();
 
-
-
-
-
-
-
-
-export default categoryRouter
+categoryRouter
+  .route("/")
+  .post(upload.single("file"), createCategory)
+  .get(getAllCategory);
+categoryRouter
+  .route("/:id")
+  .get(getOneCategory)
+  .patch(upload.single("file"), updateCategory)
+  .delete(deleteCategory);
+export default categoryRouter;
