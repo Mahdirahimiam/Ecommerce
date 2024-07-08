e7078979efddb961554664c008756a8d328ebe98
AmirrezaMirshafiee <162035710+AmirrezaMirshafiee@users.noreply.github.com>
2024-07-08 10:34:23 +0330

    Update slider.js

--- Routes/slider.js
+++ Routes/slider.js
@@ -1,11 +1,21 @@
 import express from "express";
+import {
+  createSlider,
+  deleteSlider,
+  getAllSlider,
+  getOneSlider,
+  updateSlider,
+} from "../Controllers/sliderCn.js";
+import upload from "../Utils/UploadFile.js";
 const sliderRouter = express.Router();
 
-
-
-
-
-
-
-
-export default sliderRouter
+sliderRouter
+  .route("/")
+  .get(getAllSlider)
+  .post(upload.single("file"), createSlider);
+sliderRouter
+  .route("/:id")
+  .get(getOneSlider)
+  .patch(upload.single("file"), updateSlider)
+  .delete(deleteSlider);
+export default sliderRouter;
